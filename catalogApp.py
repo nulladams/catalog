75b97d2c8e703976de79ff603a1c57436221dbda
Leonardo Adams <leonardoadams@gmail.com>
2018-08-28 18:35:48 -0300

    Create models and database

--- catalogApp.py
+++ catalogApp.py
@@ -1,11 +1,13 @@
-from flask import Flask, jsonify, request, url_for, abort, g, render_template
+from flask import Flask, jsonify, request, url_for
+from flask import abort, g, render_template
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 import random, string
-from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
+from itsdangerous import (TimedJSONWebSignatureSerializer
+                          as Serializer, BadSignature,
+                          SignatureExpired)
 from flask_httpauth import HTTPBasicAuth
-
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
 from flask import make_response
@@ -13,44 +15,51 @@ import requests
 import httplib2
 
 
+
 app = Flask(__name__)
 
+#Show catalof of categories and lasts items added
 @app.route("/")
 def showCatalog():
     return "Catalog"
 
+#Show items in a category
 @app.route("/catalog/<category>/items")
 def showItems(category):
     return category
 
+#Add new items
 @app.route("/catalog/items/new")
 def newItem():
     return "New Item"
 
-
+#Show an item
 @app.route("/catalog/<category>/items/<item>")
 def showItem(category, item):
     return "Show Item: %s, %s" % (category, item)
 
+#Edit an item
 @app.route("/catalog/<item>/edit")
 def editItem(item):
     return "Edit Item: %s" % (item)
 
+#Delete an item
 @app.route("/catalog/<item>/delete")
 def deleteItem(item):
     return "Delete Item: %s" % (item)
 
+#Show login page
 @app.route("/login")
 def showLogin():
     return ("Login")
 
+#Provide JSON endpoint for categories and items
 @app.route("/catalog.json")
 def showCatalogItems():
     return "JSON"
 
 
-
+#Start application
 if __name__ == '__main__':
     app.debug = True
-    #app.config['SECRET_KEY'] = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
     app.run(host='0.0.0.0', port=8000)
---
+++ models.py
@@ -0,0 +1,85 @@
+from sqlalchemy import Column,Integer,String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+from passlib.apps import custom_app_context as pwd_context
+import random, string
+from itsdangerous import(TimedJSONWebSignatureSerializer
+                         as Serializer, BadSignature,
+                         SignatureExpired)
+
+Base = declarative_base()
+
+#secret key to create and verify tokens
+secret_key = ''.join(random.choice(string.ascii_uppercase
+                                   + string.digits)
+                                   for x in range(32))
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(32))
+    picture = Column(String)
+    email = Column(String, index=True)
+    password_hash = Column(String(64))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    #Add a method to generate auth tokens here
+    def generate_auth_token(self, expiration=600):
+        s = Serializer(secret_key, expires_in=expiration)
+        return s.dumps({'id' : self.id})
+
+    #Add a method to verify auth tokens here
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(secret_key)
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return None
+        except BadSignature:
+            return None
+        user_id = data['id']
+        return user_id
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+
+    @property
+    def serialize(self):
+        return {
+            'id' : self.name,
+            'name' : self.name,
+        }
+
+
+class Item(Base):
+    __tablename__ = 'item'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    description = Column(String)
+    cat_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+
+    @property
+    def serialize(self):
+        return {
+            'id' : self.id,
+            'title' : self.title,
+            'description' : self.description,
+            'cat_id' : self.cat_id
+        }
+
+engine = create_engine('sqlite:///catalog.db')
+
+Base.metadata.create_all(engine)
