64281aac6610487eb1669b3e65e27e675664111e
Leonardo Adams <leonardoadams@gmail.com>
2018-09-14 12:02:40 -0300

    Create CRUD for catalog items

--- catalogApp.py
+++ catalogApp.py
@@ -1,7 +1,5 @@
-from flask import Flask, jsonify, request, url_for
+from flask import Flask, jsonify, request, url_for, redirect
 from flask import abort, g, render_template
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 import random, string
 from itsdangerous import (TimedJSONWebSignatureSerializer
@@ -13,40 +11,112 @@ from oauth2client.client import FlowExchangeError
 from flask import make_response
 import requests
 import httplib2
-
-
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base, Category, Item, User
 
 app = Flask(__name__)
 
 #Show catalof of categories and lasts items added
 @app.route("/")
+@app.route("/catalog")
 def showCatalog():
-    return "Catalog"
+    engine = create_engine('sqlite:///catalog.db')
+    Base.metadata.bind = engine
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    categories = session.query(Category).all()
+    items = session.query(Item).all()
+    engine.dispose()
+    return render_template('catalog.html', categories=categories, items=items)
 
 #Show items in a category
-@app.route("/catalog/<category>/items")
-def showItems(category):
-    return category
+@app.route("/catalog/<int:category>/items")
+def showItems(category_id):
+    return category_id
 
 #Add new items
-@app.route("/catalog/items/new")
+@app.route("/catalog/items/new", methods=['GET','POST'])
 def newItem():
-    return "New Item"
+    if request.method == 'POST':
+
+        engine = create_engine('sqlite:///catalog.db')
+        Base.metadata.bind = engine
+        DBSession = sessionmaker(bind=engine)
+        session = DBSession()
+        newItem = Item(title=request.form['title'],
+                       description=request.form['description'],
+                       cat_id=request.form['category'])
+        print(newItem.title)
+        print(newItem.description)
+        print(newItem.cat_id)
+        session.add(newItem)
+        session.commit()
+        engine.dispose()
+        return redirect(url_for('showCatalog'))
+
+    if request.method == 'GET':
+        engine = create_engine('sqlite:///catalog.db')
+        Base.metadata.bind = engine
+        DBSession = sessionmaker(bind=engine)
+        session = DBSession()
+        categories = session.query(Category).all()
+        engine.dispose()
+        return render_template('newitem.html', categories=categories)
 
 #Show an item
-@app.route("/catalog/<category>/items/<item>")
-def showItem(category, item):
-    return "Show Item: %s, %s" % (category, item)
+@app.route("/catalog/<int:category_id>/items/<int:item_id>")
+def showItem(category_id, item_id):
+    engine = create_engine('sqlite:///catalog.db')
+    Base.metadata.bind = engine
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    item = session.query(Item).filter_by(id=item_id).one()
+    engine.dispose()
+    # return "Show Item: %s, %s" % (category, item)
+    return render_template('showitem.html', item=item)
 
 #Edit an item
-@app.route("/catalog/<item>/edit")
-def editItem(item):
-    return "Edit Item: %s" % (item)
+@app.route("/catalog/<int:item_id>/edit", methods=['GET','POST'])
+def editItem(item_id):
+    engine = create_engine('sqlite:///catalog.db')
+    Base.metadata.bind = engine
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    if request.method == 'GET':
+        item = session.query(Item).filter_by(id=item_id).one()
+        categories = session.query(Category).all()
+        engine.dispose()
+        return render_template('edititem.html',
+                                item=item,
+                                categories=categories)
+    if request.method == 'POST':
+        item = session.query(Item).filter_by(id=item_id).one()
+        item.title = request.form['title']
+        item.description = request.form['description']
+        item.cat_id = request.form['category']
+        session.add(item)
+        session.commit()
+        engine.dispose()
+        return redirect(url_for('showCatalog'))
 
 #Delete an item
-@app.route("/catalog/<item>/delete")
-def deleteItem(item):
-    return "Delete Item: %s" % (item)
+@app.route("/catalog/<int:item_id>/delete", methods=['GET','POST'])
+def deleteItem(item_id):
+    engine = create_engine('sqlite:///catalog.db')
+    Base.metadata.bind = engine
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    item = session.query(Item).filter_by(id=item_id).one()
+    if request.method == 'GET':
+        engine.dispose()
+        return render_template('deleteitem.html', item=item)
+    if request.method == 'POST':
+        session.delete(item)
+        session.commit()
+        engine.dispose()
+        #return "Delete Item: %s" % (item_id)
+        return redirect(url_for('showCatalog'))
 
 #Show login page
 @app.route("/login")
